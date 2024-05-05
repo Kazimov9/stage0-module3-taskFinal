9361cd7d62c2e05d020d05c4c69a80b3d1bb1e72
Miraziz <kazimovmiraziz197@gmail.com>
2024-05-05 16:46:06 +0500

    Tasks are solved

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int n,sum = 0;
+        while (number > 0) {
+            n = number % 10;
+            sum = sum + n;
+            number = number/10;
+        }
+        System.out.println(sum);
     }
 }
